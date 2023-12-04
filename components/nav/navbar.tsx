027428e00e834452dbc8b400c47ae5e7312dbd6c
theonrex <theonrex1@gmail.com>
2023-12-04 17:01:49 +0100

    fixed deployent erro

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -12,11 +12,11 @@ export default function Navbar() {
           href="#"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img
+          {/* <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8"
             alt="Flowbite Logo"
-          />
+          /> */}
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             WeaveSwap
           </span>
